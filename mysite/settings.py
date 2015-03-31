094eab0648f661c95a73055ddd9fc569b891d7ad
PegasusWang <tianma201211@gmail.com>
2015-03-31 17:08:14 +0800

    change settings.py add APP

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'physics',    # my project
 )
 
 MIDDLEWARE_CLASSES = (
@@ -66,9 +67,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
