832c9d510151a8927ee0829d7410fadaa6aa9f78
PegasusWang <tianma201211@gmail.com>
2015-03-31 18:45:11 +0800

    add mysql settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,8 +59,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'physics',
+        'USER': 'wnn',
+        'PASSWORD': 'wnnwnn',
     }
 }
 
