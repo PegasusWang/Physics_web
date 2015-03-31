5bcd62f4de1093e8f1732b29b3137aab3963f525
PegasusWang <tianma201211@gmail.com>
2015-03-31 17:27:04 +0800

    add .gitignore

--- physics/admin.py
+++ physics/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from physics.models import Teacher, Student, Question
 
 # Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Question)
