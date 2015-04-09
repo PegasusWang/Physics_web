272e10df307db3e12514af203dc2b812ddc4fca7
Pegasus Wang <tianma201211@gmail.com>
2015-04-10 07:52:27 +0800

    add login view

--- physics/views.py
+++ physics/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+# Author: Pegasus Wang (pegasuswang@qq.com, http://ningning.today)
 
-# Create your views here.
+# Description: Android backend views and frontend show.
+
+"""
+from django.shortcuts import render
+from django.http import HttpResponse
+from models import Student, Question, Notification
+
+
+def login(request):
+    name = request.POST.get(u'usernum')
+    password = request.POST.get(u'paswd')
+    flag = u'1'
+    if Student.objects.get(stu_id=name):
+        flag = u'1'
+    else:
+        flag = u'-1'
+    return HttpResponse(flag, content_type=u'text/html;charset=utf-8', status=200)
