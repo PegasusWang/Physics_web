2b389c9cd238c909b9d58cece1da3d442e9f10fa
Pegasus Wang <tianma201211@gmail.com>
2015-04-12 10:46:38 +0800

    views.py add login and register

--- physics/views.py
+++ physics/views.py
@@ -8,46 +8,69 @@
 """
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+
 from models import Student, Question, Notification
 
 
+@csrf_exempt
 def login(request):
     """process android LoginActivity post"""
-    name = request.POST.get(u'usernum')
-    password = request.POST.get(u'paswd')
-    flag = u'1'
-    if Student.objects.get(stu_id=name):
-        flag = u'1'
+    stu_id = request.POST.get(u'usernum')
+    password = request.POST.get(u'pswd')
+    login_success = u'1'
+    login_fail = u'-1'
+
+    try:
+        p = Student.objects.get(stu_id=stu_id, password=password)
+
+    except Student.DoesNotExist:
+        print 'login_fail'
+        return HttpResponse(login_fail, content_type=u'text/html;charset=utf-8',
+                            status=200)
     else:
-        flag = u'-1'
-    return HttpResponse(flag, content_type=u'text/html;charset=utf-8',
-                        status=200)
+        print 'login_success'
+        return HttpResponse(login_success, content_type=u'text/html;charset=utf-8',
+                            status=200)
 
 
+@csrf_exempt    # note: for post method use csrf_exempt or 403 error
 def register(request):
     """process android RegisterActivity post"""
-    stu_id = request.POST.get(u'usernum')
     name = request.POST.get(u'username')
+    stu_id = request.POST.get(u'usernum')
     password = request.POST.get(u'pswd')
-    if Student.object.get(stu_id=stu_id):
-        # if user_exist, return success but do not modify database
-        return HttpResponse('user_exist', content_type=u'text/html;charset=utf-8',
+    reg_success = u'stu_id=%s name=%s password=%s' % (stu_id, name, password)
+
+    try:
+        p = Student.objects.get(stu_id=stu_id)
+
+    except Student.DoesNotExist:
+        print reg_success
+        Student.objects.create(stu_id=stu_id, name=name, password=password)
+        return HttpResponse(reg_success, content_type=u'text/html;charset=utf-8',
                             status=200)
     else:
-        # create new stuent user
-        Student.objects.create(stu_id=stu_id, name=name, password=password)
-        return HttpResponse('reg_success', content_type=u'text/html;charset=utf-8',
+        print u'stu_id=%s already exists' % stu_id
+        return HttpResponse('user_exist', content_type=u'text/html;charset=utf-8',
                             status=200)
+
+def show_question(request):
+    """process Android ShowAllQuestionActivity GET"""
+    json_queryset = serializers.serialize('json', Question.objects.all())
+    print json_queryset
+    print type(json_queryset)
+    #return HttpResponse(json_queryset, content_type=u'text/html;charset=utf-8',
+                        #status=200)
 
 
 def res_notice(request):
     # TODO
-
-
-def show_questin(request):
-    # TODO
+    pass
 
 
 def upload_answer(request):
     # TODO
+    pass
 
