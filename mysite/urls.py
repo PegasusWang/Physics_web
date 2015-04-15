9974f4f4c63e12b146327cc8f5d123cfdc9e1678
Pegasus Wang <tianma201211@gmail.com>
2015-04-15 09:20:55 +0800

    change url user to users for restful

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^users/', include('physics.urls', namespace='physics')),
+    url(r'^admin/', include(admin.site.urls)),
 )
